f45674c97c14d59ae0faf406c7535a5d4295c43e
Nguyen The Phuong <thephuongcqt@gmail.com>
2017-11-28 20:59:17 +0700

    Add firebase SDK

--- ChatFirebase/AppDelegate.swift
+++ ChatFirebase/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         
         window?.rootViewController = UINavigationController(rootViewController: ViewController())
-        
+        FIRApp.configure()
         // Override point for customization after application launch.
         return true
     }
