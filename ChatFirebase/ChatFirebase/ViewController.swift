556dd9b85b1d22c871b161da2323679452721d61
Nguyen The Phuong <thephuongcqt@gmail.com>
2017-11-28 18:35:43 +0700

    Initialize project

---
+++ ChatFirebase/ChatFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatFirebase
+//
+//  Created by Nguyen The Phuong on 11/28/17.
+//  Copyright © 2017 Nguyen The Phuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
